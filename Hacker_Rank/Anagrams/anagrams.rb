bc2467cba0875e0214bdc522a3409e0a2440e840
anthony-ng <thomsonng123@gmail.com>
2015-03-09 02:42:49 -0700

    added init solution

--- Hacker_Rank/Anagrams/anagrams.rb
+++ Hacker_Rank/Anagrams/anagrams.rb
@@ -1,3 +1,16 @@
 def check_anagrams(first_words, second_words)
+  counter = 0
 
+  first_words.length.times do
+    first_words[i].downcase!
+    second_words[i].downcase!
+
+    if first_words[i].split(//).sort == second_words[i].split(//).sort
+      puts 1
+    else
+      puts 0
+    end
+
+    counter += 1
+  end
 end
