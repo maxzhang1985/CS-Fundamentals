18805dbac460a6ebe30905ffda03bd918b8ab67e
anthony-ng <thomsonng123@gmail.com>
2015-03-09 02:48:13 -0700

    updated test case

--- Hacker_Rank/Anagrams/anagrams.rb
+++ Hacker_Rank/Anagrams/anagrams.rb
@@ -1,21 +1,24 @@
 def check_anagrams(first_words, second_words)
   counter = 0
+  result = []
 
   first_words.length.times do
     first_words[counter].downcase!
     second_words[counter].downcase!
 
-    if first_words[counter].split(//).sort == second_words[icounter].split(//).sort
-      puts 1
+    if first_words[counter].split(//).sort == second_words[counter].split(//).sort
+      result << 1
     else
-      puts 0
+      result << 0
     end
 
     counter += 1
   end
+
+  return result
 end
 
 # Test Case
 first = ["cinema", "host", "aba", "train"]
 second = ["iceman", "shot", "bab", "rain"]
-check_anagrams(first, second) == "1\n1\n0\n0"
+puts check_anagrams(first, second) == [1,1,0,0]
