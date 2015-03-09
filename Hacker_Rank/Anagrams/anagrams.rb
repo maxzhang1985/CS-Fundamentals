ad31fa092da8ba179323df1f183e5c969fed7bd7
anthony-ng <thomsonng123@gmail.com>
2015-03-09 02:44:37 -0700

    added test case

--- Hacker_Rank/Anagrams/anagrams.rb
+++ Hacker_Rank/Anagrams/anagrams.rb
@@ -2,10 +2,10 @@ def check_anagrams(first_words, second_words)
   counter = 0
 
   first_words.length.times do
-    first_words[i].downcase!
-    second_words[i].downcase!
+    first_words[counter].downcase!
+    second_words[counter].downcase!
 
-    if first_words[i].split(//).sort == second_words[i].split(//).sort
+    if first_words[counter].split(//).sort == second_words[icounter].split(//).sort
       puts 1
     else
       puts 0
@@ -14,3 +14,8 @@ def check_anagrams(first_words, second_words)
     counter += 1
   end
 end
+
+# Test Case
+first = ["cinema", "host", "aba", "train"]
+second = ["iceman", "shot", "bab", "rain"]
+check_anagrams(first, second) == "1\n1\n0\n0"
