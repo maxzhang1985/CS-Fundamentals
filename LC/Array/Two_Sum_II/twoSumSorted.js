9d89354d5c1ae069d6a96510bd8d4ddbfa4584d1
anthony-ng <thomsonng123@gmail.com>
2015-03-08 23:29:53 -0700

    remove description from code

--- LC/Array/Two_Sum_II/twoSumSorted.js
+++ LC/Array/Two_Sum_II/twoSumSorted.js
@@ -1,12 +1,3 @@
-// Given an array of integers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
-
-// The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.
-
-// You may assume that each input would have exactly one solution.
-
-// Input: numbers={2, 7, 11, 15}, target=9
-// Output: index1=1, index2=2
-
 // #1: Binary Search, Time: O(nlogn), Space: O(1)
 function twoSum(numbers, target) {
   for (var first = 0; first < numbers.length; first++) {
