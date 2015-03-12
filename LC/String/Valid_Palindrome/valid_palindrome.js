4b408af2c0918927da133c89af48450f566c2523
anthony-ng <thomsonng123@gmail.com>
2015-03-11 23:32:07 -0700

    updated test case: set empty string as valid palindrome as well

--- LC/String/Valid_Palindrome/valid_palindrome.js
+++ LC/String/Valid_Palindrome/valid_palindrome.js
@@ -1,6 +1,6 @@
 function isPalindrome(input_string) {
   if (input_string.length === 0) {
-    return false;
+    return true;
   } else {
     for (var i = 0; i < input_string.length; i++) {
       // do sth
@@ -18,3 +18,4 @@ function isPalindrome(input_string) {
 // Test Case
 console.log(isPalindrome("A man, a plan, a canal: Panama") === true);
 console.log(isPalindrome("race a car") === false);
+console.log(isPalindrome("") == true);
