93af7ebc4904cac57277f8d89327538cc58ea2a6
anthony-ng <thomsonng123@gmail.com>
2015-03-11 23:27:18 -0700

    created init isPalindrome function

--- LC/String/Valid_Palindrome/valid_palindrome.js
+++ LC/String/Valid_Palindrome/valid_palindrome.js
@@ -1,4 +1,14 @@
-// Test Case
+function isPalindrome(input_string) {
+  // do sth
 
+}
+
+
+
+
+
+
+
+// Test Case
 console.log(isPalindrome("A man, a plan, a canal: Panama") === true);
 console.log(isPalindrome("race a car") === false);
