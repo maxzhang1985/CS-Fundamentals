77d0eb3a5ae5be065adb4578291cd3b49b354e70
anthony-ng <thomsonng123@gmail.com>
2015-03-11 23:30:20 -0700

    added iteration

--- LC/String/Valid_Palindrome/valid_palindrome.js
+++ LC/String/Valid_Palindrome/valid_palindrome.js
@@ -1,5 +1,11 @@
 function isPalindrome(input_string) {
-  // do sth
+  if (input_string.length === 0) {
+    return false;
+  } else {
+    for (var i = 0; i < input_string.length; i++) {
+      // do sth
+    };
+  }
 
 }
 
