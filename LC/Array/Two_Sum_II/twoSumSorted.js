5a4aee752f741939786329fe84f2d7116ac629de
anthony-ng <thomsonng123@gmail.com>
2015-03-13 00:47:04 -0700

    added two solution: two pointer

--- LC/Array/Two_Sum_II/twoSumSorted.js
+++ LC/Array/Two_Sum_II/twoSumSorted.js
@@ -1,32 +1,39 @@
-// #1: Binary Search, Time: O(nlogn), Space: O(1)
+// #1: Binary Search, Runtime: O(nlogn), Space: O(1)
+// function twoSum(numbers, target) {
+//   for (var first = 0; first < numbers.length; first++) {
+//     var second = bsearch(numbers, target - numbers[first], first + 1);
+//     if (second != -1) {
+//       console.log(numbers[first] + "+" + numbers[second] + "=" + target);
+//       return [first + 1, second + 1];
+//     }
+//   }
+//   console.log("No two sum solution");
+// }
+
+// function bsearch(list, key, startPt) {
+//   var left = startPt;           //0
+//   var right = list.length - 1;  //3
+//   while (left < right) {
+//     var mid = (left + right) / 2;
+//     if (list[mid] < key) {
+//       left = mid + 1;
+//     } else {
+//       right = mid;
+//     }
+//   }
+//   if (left == right && list[left] == key) {
+//     return left
+//   } else {
+//     return -1
+//   }
+// }
+
+// #2: Two Pointers, Runtime: O(n), Space: O(1)
 function twoSum(numbers, target) {
-  for (var first = 0; first < numbers.length; first++) {
-    var second = bsearch(numbers, target - numbers[first], first + 1);
-    if (second != -1) {
-      console.log(numbers[first] + "+" + numbers[second] + "=" + target);
-      return [first + 1, second + 1];
-    }
-  }
-  console.log("No two sum solution");
+  // do sth
 }
 
-function bsearch(list, key, startPt) {
-  var left = startPt;           //0
-  var right = list.length - 1;  //3
-  while (left < right) {
-    var mid = (left + right) / 2;
-    if (list[mid] < key) {
-      left = mid + 1;
-    } else {
-      right = mid;
-    }
-  }
-  if (left == right && list[left] == key) {
-    return left
-  } else {
-    return -1
-  }
-}
+
 
 // Test Case
 numbers = [2,4,11,15]
