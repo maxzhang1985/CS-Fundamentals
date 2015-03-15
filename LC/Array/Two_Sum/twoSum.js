7f8acc441cf85624285a72d0c1fc3288f2d28115
anthony-ng <thomsonng123@gmail.com>
2015-03-14 23:36:01 -0700

    updated test case to be not zero-based

--- LC/Array/Two_Sum/twoSum.js
+++ LC/Array/Two_Sum/twoSum.js
@@ -15,11 +15,11 @@ function twoSum(numbers, target) {
 // Test Case
 numbers = [2,4,11,15]
 target = 19
-console.log(twoSum(numbers,target)[0] === 1 && twoSum(numbers,target)[1] === 3);
+console.log(twoSum(numbers,target)[0] === 2 && twoSum(numbers,target)[1] === 4);
 
 numbers = [3,15,37,22,48]
 target = 85
-console.log(twoSum(numbers,target)[0] === 2 && twoSum(numbers,target)[1] === 4);
+console.log(twoSum(numbers,target)[0] === 3 && twoSum(numbers,target)[1] === 5);
 
 numbers = [3,24,53,4]
 target = 100
