cabb5bd7e63fcc33110bf6399f3c9840a244fdb0
anthony-ng <thomsonng123@gmail.com>
2015-03-14 23:37:18 -0700

    added comment

--- LC/Array/Two_Sum/twoSum.js
+++ LC/Array/Two_Sum/twoSum.js
@@ -15,12 +15,12 @@ function twoSum(numbers, target) {
 // Test Case
 numbers = [2,4,11,15]
 target = 19
-console.log(twoSum(numbers,target)[0] === 2 && twoSum(numbers,target)[1] === 4);
+console.log(twoSum(numbers,target)[0] === 2 && twoSum(numbers,target)[1] === 4); // true
 
 numbers = [3,15,37,22,48]
 target = 85
-console.log(twoSum(numbers,target)[0] === 3 && twoSum(numbers,target)[1] === 5);
+console.log(twoSum(numbers,target)[0] === 3 && twoSum(numbers,target)[1] === 5); // true
 
 numbers = [3,24,53,4]
 target = 100
-console.log(twoSum(numbers,target) === 'No two sum solution')
+console.log(twoSum(numbers,target) === 'No two sum solution'); // true
