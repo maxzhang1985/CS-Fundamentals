6f29b17ab8000330f76f7752b26424b4ef9beadc
anthony-ng <thomsonng123@gmail.com>
2015-03-17 23:49:46 -0700

    added space O(1) soln

--- LC/Array/Remove_Duplicates_from_Sorted_Array/removeDuplicates.js
+++ LC/Array/Remove_Duplicates_from_Sorted_Array/removeDuplicates.js
@@ -1,17 +1,22 @@
 function removeDuplicates (array) {
   // Runtime: O(n); Space: O(n)
-  var list = [];
-  for (var i = array.length - 1; i >= 0; i--) {
-    // Check if specific value exists in list
-    if (list.indexOf(array[i]) === -1) {
-      list.splice(array[i], 0, array[i]);
-    } else {
-      // Remove from array if pair exists
-      list.splice(list.indexOf(array[i]), 1);
-      array.splice(i, 1);
-    }
-  }
-  return array.length;
+  // var list = [];
+  // for (var i = array.length - 1; i >= 0; i--) {
+  //   // Check if specific value exists in list
+  //   if (list.indexOf(array[i]) === -1) {
+  //     list.splice(array[i], 0, array[i]);
+  //   } else {
+  //     // Remove from array if pair exists
+  //     list.splice(list.indexOf(array[i]), 1);
+  //     array.splice(i, 1);
+  //   }
+  // }
+  // return array.length;
+
+// ========================================================
+// Runtime: O(n); Space: O(1)
+
+
 }
 
 // Test Case
