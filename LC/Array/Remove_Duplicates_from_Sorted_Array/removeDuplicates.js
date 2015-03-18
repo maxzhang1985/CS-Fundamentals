04ea202963543bc11585bd640d7177cffadddcea
anthony-ng <thomsonng123@gmail.com>
2015-03-18 00:02:07 -0700

    updated test case: array with multiple duplicates

--- LC/Array/Remove_Duplicates_from_Sorted_Array/removeDuplicates.js
+++ LC/Array/Remove_Duplicates_from_Sorted_Array/removeDuplicates.js
@@ -1,17 +1,18 @@
 function removeDuplicates (array) {
   // Runtime: O(n); Space: O(n)
-  // var list = [];
-  // for (var i = array.length - 1; i >= 0; i--) {
-  //   // Check if specific value exists in list
-  //   if (list.indexOf(array[i]) === -1) {
-  //     list.splice(array[i], 0, array[i]);
-  //   } else {
-  //     // Remove from array if pair exists
-  //     list.splice(list.indexOf(array[i]), 1);
-  //     array.splice(i, 1);
-  //   }
-  // }
-  // return array.length;
+//   var list = [];
+//   for (var i = array.length - 1; i >= 0; i--) {
+//     // Check if specific value exists in list
+//     if (list.indexOf(array[i]) === -1) {
+//       list.splice(array[i], 0, array[i]);
+//     } else {
+//       // Remove from array if pair exists
+//       list.splice(list.indexOf(array[i]), 1);
+//       array.splice(i, 1);
+//     }
+//   }
+//   return array.length;
+// }
 
 // ========================================================
   // Runtime: O(n); Space: O(1)
@@ -38,5 +39,5 @@ function removeDuplicates (array) {
 A = [1,1,2]
 console.log(removeDuplicates(A)); // 2
 
-B = [3,3,5,5,5,7,7,14,35,46,46,79]
-console.log(removeDuplicates(B)); // 7
+B = [3,3,3,5,5,7,7,7,7,9,9,9,9,9,11]
+console.log(removeDuplicates(B)); // 5
