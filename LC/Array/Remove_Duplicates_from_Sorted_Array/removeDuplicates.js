1a5d44929cd98bedacce913dbe62e5f73082b97f
anthony-ng <thomsonng123@gmail.com>
2015-03-17 22:44:06 -0700

    added comment

--- LC/Array/Remove_Duplicates_from_Sorted_Array/removeDuplicates.js
+++ LC/Array/Remove_Duplicates_from_Sorted_Array/removeDuplicates.js
@@ -1,16 +1,22 @@
 function removeDuplicates (array) {
   var list = [];
   for (var i = 0; i < array.length; i++) {
-    if (list[array[i]] === nil) {
-      list[array[i]] = array[i];
+    // Check if array is not empty
+    if (!list[array[i]]) {
+      list.splice(array[i], 0, array[i]);
+      console.log(list);
+      console.log(array[i]);
     } else {
       list.splice(array[i], 1);
+      console.log(list);
+      console.log(array[i]);
     }
   }
+  return list;
 }
 
 
 
 // Test Case
 A = [1,1,2]
-console.log(removeDuplicates(A) == 2);
+console.log(removeDuplicates(A));
