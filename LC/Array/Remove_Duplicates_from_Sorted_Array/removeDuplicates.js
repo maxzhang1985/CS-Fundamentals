e1ba63d8a5478e8a8cdb815a32a76be619e8c714
anthony-ng <thomsonng123@gmail.com>
2015-03-17 23:13:17 -0700

    update the for loop to backward

--- LC/Array/Remove_Duplicates_from_Sorted_Array/removeDuplicates.js
+++ LC/Array/Remove_Duplicates_from_Sorted_Array/removeDuplicates.js
@@ -1,13 +1,13 @@
 function removeDuplicates (array) {
   // Runtime: O(n); Space: O(n)
   var list = [];
-  for (var i = 0; i < array.length; i++) {
+  for (var i = array.length; i <= 0; i--) {
     // Check if specific value exists in list
     if (list.indexOf(array[i]) === -1) {
       list.splice(array[i], 0, array[i]);
     } else {
-      // Remove from list if pair exists
-      list.splice(list.indexOf(array[i]), 1);
+      // Remove from array if pair exists
+      array.splice(array[i]), 1);
     }
   }
   array.splice(array.indexOf(list[0]), 1);
