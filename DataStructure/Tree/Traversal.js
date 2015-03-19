36be824f1b578a577f95aa2446aaef03b7e193bd
anthony-ng <thomsonng123@gmail.com>
2015-03-19 04:28:59 -0700

    added test case for inOrder

--- DataStructure/Tree/Traversal.js
+++ DataStructure/Tree/Traversal.js
@@ -7,7 +7,17 @@ function inOrder(node) {
   }
 }
 
-
+// Test Case for inOrder
+var nums = new BST();
+nums.insert(23);
+nums.insert(45);
+nums.insert(16);
+nums.insert(37);
+nums.insert(3);
+nums.insert(99);
+nums.insert(22);
+print("Inorder traversal: ");
+inOrder(nums.root);
 
 // Pre-order traversal (root node 1st, then left to right)
 function preOrder(node) {
