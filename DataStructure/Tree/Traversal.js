6dd48a7010ff8ec368b09f71453c62ca41940b64
anthony-ng <thomsonng123@gmail.com>
2015-03-19 04:15:42 -0700

    set up all 3 dsf traversal function

---
+++ DataStructure/Tree/Traversal.js
@@ -0,0 +1,16 @@
+// In-order traversal (ascending order of node key values)
+function inOrder(node) {
+
+}
+
+
+
+// Pre-order traversal (root node 1st, then left to right)
+function preOrder(node) {
+  //sth
+}
+
+// Post-order traversal (child nodes first, left to right)
+function postOrder(node) {
+  //sth
+}
