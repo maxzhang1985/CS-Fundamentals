bbc1284229ff05069d5116dfabfdd5ae2e59c9c8
Anthony Ng <thomsonng123@gmail.com>
2016-04-13 18:52:17 -0700

    create testCase function

--- LC/Array/Two_Sum/twoSum.js
+++ LC/Array/Two_Sum/twoSum.js
@@ -13,32 +13,25 @@ function twoSum(numbers, target) {
 }
 
 // Test Case
+function testCase(numbers, target) {
+  twoSum(numbers, target)
+  console.log('Input=> numbers: ' + numbers + ' | target: ' + target);
+  console.log('Output=> index1 = ' + (twoSum(numbers, target)[0] + 1) + ', index2 = ' + (twoSum(numbers, target)[1] + 1));
+  if (twoSum(numbers,target) != "No two sum solution") {
+    console.log(numbers[twoSum(numbers,target)[0]] + ' + ' + numbers[twoSum(numbers,target)[1]] + ' = ' + target + '\n'); //19
+  } else {
+    console.log("No two sum solution");
+  }
+}
+
 var numbers = [2,4,11,15]
 var target = 19
-console.log('Input=> numbers: ' + numbers + ' | target: ' + target);
-console.log('Output=> index1 = ' + (twoSum(numbers, target)[0] + 1) + ', index2 = ' + (twoSum(numbers, target)[1] + 1));
-if (twoSum(numbers,target) != "No two sum solution") {
-  console.log(numbers[twoSum(numbers,target)[0]] + ' + ' + numbers[twoSum(numbers,target)[1]] + ' = ' + target + '\n'); //19
-} else {
-  console.log("No two sum solution");
-}
+testCase(numbers, target)
 
 var numbers = [3,15,37,22,48]
 var target = 85
-console.log('numbers: ' + numbers + ' | target: ' + target);
-console.log('Output=> index1 = ' + (twoSum(numbers, target)[0] + 1) + ', index2 = ' + (twoSum(numbers, target)[1] + 1));
-if (twoSum(numbers,target) != "No two sum solution") {
-  console.log(numbers[twoSum(numbers,target)[0]] + ' + ' + numbers[twoSum(numbers,target)[1]] + ' = ' + target + '\n'); //85
-} else {
-  console.log("No two sum solution");
-}
+testCase(numbers, target)
 
 var numbers = [3,24,53,4]
 var target = 100
-console.log('numbers: ' + numbers + ' | target: ' + target);
-console.log('Output=> index1 = ' + (twoSum(numbers, target)[0] + 1) + ', index2 = ' + (twoSum(numbers, target)[1] + 1));
-if (twoSum(numbers,target) != "No two sum solution") {
-  console.log(numbers[twoSum(numbers,target)[0]] + ' + ' + numbers[twoSum(numbers,target)[1]] + ' = ' + target + '\n');
-} else {
-  console.log("No two sum solution");
-}
+testCase(numbers, target)
